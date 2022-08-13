18abcfc2607bd298c7233d06a92a576269007bfe
sunhak20 <sunhak20@naver.com>
2022-08-13 17:04:48 +0900

    Initial commit

---
+++ src/main/java/com/example/shopping/SpringShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringShoppingApplication.class, args);
+    }
+
+}
